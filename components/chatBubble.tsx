845ad715f44cd57d7c27b64ce635f516c9230398
kkhan0214 <70661595+kkhan0214@users.noreply.github.com>
2025-05-31 00:46:38 +0900

    Update chatBubble.tsx

--- components/chatBubble.tsx
+++ components/chatBubble.tsx
@@ -7,7 +7,7 @@ const ChatBubble = ({ from, text }: Props) => {
   const isBot = from === 'bot';
   return (
     <div className={`flex ${isBot ? 'justify-start' : 'justify-end'}`}>
-      <div className={`px-4 py-2 rounded-xl max-w-xs ${isBot ? 'bg-gray-300' : 'bg-purple-400 text-white'}`}>
+      <div className={`px-4 py-2 rounded-xl max-w-xs break-words ${isBot ? 'bg-gray-300' : 'bg-purple-500 text-white'}`}>
         {text}
       </div>
     </div>
